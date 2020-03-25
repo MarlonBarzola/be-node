952f7e39fe641e371ad1a18715737659e3586167
Marlon <marlonbarzola@hotmail.com>
2020-03-25 15:20:20 -0500

    Rutas protegidas para adminsitradores

--- server/routes/voto.js
+++ server/routes/voto.js
@@ -5,6 +5,10 @@ const { verificaToken, verificaAdminRole } = require('../middlewares/autenticaci
 
 const VotoController = require('../controllers/VotoController');
 
+//
+// ─── RUTAS PROTEGIDAS ───────────────────────────────────────────────────────────
+//
+
 router.get('/voto/:year/:mes', [verificaToken, verificaAdminRole], VotoController.votosPorMes);
 router.get('/voto', [ verificaToken, verificaAdminRole ], VotoController.votosPorArea);
 router.post('/voto', verificaToken, VotoController.crear);
